20c114cdd4b7c8914b2897f1b4213efa211e3979
beboo22 <mt557829@gmail.com>
2023-08-30 00:36:10 +0300

    task

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,12 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	size_t l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	size_t r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	size_t l, r;
+	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-	if (l > r)
-	{
-		return (l);
-	}
-	else
-	{
-		return (r);
-	}
+	return ((l > r) ? l : r);
 }
