dd7cc116967c977ba204efe3f16dd9f9dbecabd4
beboo22 <mt557829@gmail.com>
2023-08-29 14:59:21 +0300

    task

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,11 +8,8 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL )
+	if (node->right == NULL && node->left == NULL && node != NULL)
 		return (1);
 	else
-		return (0);
-	
-	if(node == NULL)
 		return (0);
 }
