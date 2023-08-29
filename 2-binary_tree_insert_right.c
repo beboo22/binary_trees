ff243799729e40196bafbddcb584cf11f16b7fbc
beboo22 <mt557829@gmail.com>
2023-08-29 14:56:24 +0300

    task

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,20 +15,20 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *nnode;
-    
-    if (parent == NULL)
-        return (NULL);
+	binary_tree_t *nnode;
 
-    nnode = binary_tree_node(parent,value);
-    if(nnode == NULL)
-        return (NULL);
-    
-    if(parent->right != NULL){
-        nnode->right = parent->right;
-        parent->right->parent = nnode;
-    }
-    
-    parent->right = nnode; 
-    return (parent->right); 
+	if (parent == NULL)
+		return (NULL);
+
+	nnode = binary_tree_node(parent,value);
+	if(nnode == NULL)
+		return (NULL);
+
+	if(parent->right != NULL){
+		nnode->right = parent->right;
+		parent->right->parent = nnode;
+	}
+
+	parent->right = nnode; 
+	return (parent->right); 
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -8,8 +8,11 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (node->right == NULL && node->left == NULL)
+	if (node->right == NULL && node->left == NULL )
 		return (1);
 	else
+		return (0);
+	
+	if(node == NULL)
 		return (0);
 }
