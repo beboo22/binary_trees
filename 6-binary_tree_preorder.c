7e0f597e2098231fad747526796ded0d2c3aa16b
beboo22 <mt557829@gmail.com>
2023-08-29 15:35:57 +0300

    task

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -9,7 +9,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		printf("%d\n", tree->n);
+		func(tree->n);
 		binary_tree_preorder(tree->left, func);   /* Recurse on the left subtree */
 		binary_tree_preorder(tree->right, func);  /* Recurse on the right subtree */
 	}
