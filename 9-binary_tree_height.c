b64ef6026d6c63d835d4ce3bf296721c17c28061
Ziad-Nasser <ziadnasser177@gmail.com>
2023-08-30 20:58:32 +0300

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree)
+	{
+		int left = 0, right = 0;
+
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (left > right)
+			return (left);
+		else
+			return (right);
+	}
+	else
 		return (0);
-
-	size_t l, r;
-	l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-
-	return ((l > r) ? l : r);
 }
